c3d2b6b83c720a91a109a44ecc19a795c37df892
MarcoMusiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-05-03 16:10:21 +0200

    Added "AddActionWindow"
    - Updated Design
    - Updated ResourceDictionaries
    - Updated Buttons
    - Created AddActionWindow.xaml

--- EasyBot/AddActionWindow.xaml.cs
+++ EasyBot/AddActionWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace EasyBot
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaktionslogik für AddActionWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddActionWindow : Window
     {
-        public MainWindow()
+        public AddActionWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Add_Click(object sender, RoutedEventArgs e)
-        {
-            AddActionWindow addActionWindow = new AddActionWindow();
-            addActionWindow.Show();
         }
     }
 }
