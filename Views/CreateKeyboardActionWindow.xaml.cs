a75bc0d2469c59f4df81d4a0e1797991d375068a
MarcoMusiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-05-08 00:10:49 +0200

    Added KeyBoardAction Delay function
    - Added function that only numbers can be written in the Delay TextBox

--- Views/CreateKeyboardActionWindow.xaml.cs
+++ Views/CreateKeyboardActionWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,12 @@ namespace EasyBot.Views
         public CreateKeyboardActionWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_Delay_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
