5825464dbb39935583d2166707213743f1e36e5c
Marco-Musiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-05-09 03:01:59 +0200

    Added MouseBotAction Settings functions

--- Views/MouseBotActionSettings.xaml.cs
+++ Views/MouseBotActionSettings.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -21,6 +22,9 @@ namespace EasyBot.Views
     /// </summary>
     public partial class MouseBotActionSettings : Window
     {
+
+        private bool SetMouseLocation = false;
+
         public MouseBotActionSettings(MouseBotAction mouseBotAction)
         {
             InitializeComponent();
@@ -41,11 +45,56 @@ namespace EasyBot.Views
             TextBox_Delay.Text = mouseBotAction.Delay.ToString();
 
         }
+        private void SetPosition(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
 
         private void TextBox_Delay_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+
+        private void Button_SetLocation_Click(object sender, RoutedEventArgs e)
+        {
+
+            SetMouseLocation = true;
+            Label_SetLocationInfo.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F8 && SetMouseLocation)
+            {
+
+                System.Drawing.Point MousePositionPoint = wf.Control.MousePosition;
+
+                Label_X_Value.Content = MousePositionPoint.X.ToString();
+                Label_Y_Value.Content = MousePositionPoint.Y.ToString();
+
+                Label_SetLocationInfo.Visibility = Visibility.Collapsed;
+
+                SetMouseLocation = false;
+            }
+        }
+
+
+        private void Button_TestLocation_Click(object sender, RoutedEventArgs e)
+        {
+
+            int X = Convert.ToInt32(Label_X_Value.Content);
+            int Y = Convert.ToInt32(Label_Y_Value.Content);
+
+            SetPosition(X, Y);
+
         }
     }
 }
