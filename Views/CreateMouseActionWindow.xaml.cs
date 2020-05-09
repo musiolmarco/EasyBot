0115b58a50c71cf8bfc33380abef7eb116025a5d
Marco-Musiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-05-09 04:42:02 +0200

    Removed unnecessary namespaces

--- Views/CreateMouseActionWindow.xaml.cs
+++ Views/CreateMouseActionWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using wf = System.Windows.Forms;
-using System.Drawing;
 using System.Runtime.InteropServices;
 using EasyBot.Classes;
 using System.Text.RegularExpressions;
