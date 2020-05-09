79eea1e6dcaf7090e10332c7503fee8220242b74
Marco-Musiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-05-09 03:29:34 +0200

    Added Delay TextBox function
    - Only numbers allowed again! :100:

--- Views/KeyBoardBotActionSettings.xaml.cs
+++ Views/KeyBoardBotActionSettings.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -42,6 +43,12 @@ namespace EasyBot.Views
 
             this.Close();
 
+        }
+
+        private void TextBox_Delay_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
