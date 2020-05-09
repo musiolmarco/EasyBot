58cb5d7398bb9be54107809a7e78f305ee9f0e97
Marco-Musiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-05-09 02:58:16 +0200

    Added TextBox Delay function
    - You can only write numbers in the textfield now

--- Views/MouseBotActionSettings.xaml.cs
+++ Views/MouseBotActionSettings.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -39,6 +40,12 @@ namespace EasyBot.Views
 
             TextBox_Delay.Text = mouseBotAction.Delay.ToString();
 
+        }
+
+        private void TextBox_Delay_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
